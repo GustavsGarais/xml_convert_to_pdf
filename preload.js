16dabe9ada75eea07563f7d4f4562e8d81a2a181
31DPGGarais <131352750+GustavsGarais@users.noreply.github.com>
2025-04-10 15:10:55 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,13 +1,8 @@
-const { contextBridge, ipcRenderer, dialog } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: async () => {
-    return await ipcRenderer.invoke('dialog:openFile');
-  },
-  openFolder: async () => {
-    return await ipcRenderer.invoke('dialog:openFolder');
-  },
+  openFile: () => ipcRenderer.invoke('dialog:openFile'),
+  openFolder: () => ipcRenderer.invoke('dialog:openFolder'),
   convertXml: (data) => ipcRenderer.invoke('convert-xml', data),
-  openFile: (path) => ipcRenderer.invoke('open-file', path)
-
+  openOutputFile: (path) => ipcRenderer.invoke('open-file', path)
 });
